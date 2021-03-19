d5b2e63a0cd1a417fefd066d1d3e9d52670cefd5
Shannon Young <shannon.young@sagebase.org>
2021-03-19 12:16:40 -0700

    Update travis to rename “master” -> “main”

--- travis/build.sh
+++ travis/build.sh
@@ -3,8 +3,8 @@ set -ex
 if [[ "$TRAVIS_PULL_REQUEST" != "false" ]]; then     # on pull requests
     echo "Build on PR"
     FASTLANE_EXPLICIT_OPEN_SIMULATOR=2 bundle exec fastlane test scheme:"BridgeAppExample"
-elif [[ -z "$TRAVIS_TAG" && "$TRAVIS_BRANCH" == "master" ]]; then  # non-tag commits to master branch
-    echo "Build on merge to master"
+elif [[ -z "$TRAVIS_TAG" && "$TRAVIS_BRANCH" == "main" ]]; then  # non-tag commits to main branch
+    echo "Build on merge to main"
 #    bundle exec fastlane ci_archive scheme:"BridgeAppExample" export_method:"app-store" project:"BridgeApp/BridgeApp.xcodeproj"
 elif [[ -z "$TRAVIS_TAG" && "$TRAVIS_BRANCH" =~ ^stable-.* ]]; then # non-tag commits to stable branches
     echo "Build on stable branch"
