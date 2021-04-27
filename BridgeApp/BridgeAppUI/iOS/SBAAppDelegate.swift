edb63c6f875f6d6559bba015f5fe8e9250def46c
Erin Mounts <erinys62@gmail.com>
2021-04-27 16:23:09 -0700

    Implement missing app delegate method
    needed for Bridge apps to handle BackgroundURLSession events (related to file uploads) that come in when the app is relaunched into the background

--- BridgeApp/BridgeAppUI/iOS/SBAAppDelegate.swift
+++ BridgeApp/BridgeAppUI/iOS/SBAAppDelegate.swift
@@ -70,6 +70,11 @@ open class SBAAppDelegate : RSDAppDelegate, SBBBridgeErrorUIDelegate {
         rootViewController?.contentHidden = false
     }
     
+    open func application(_ application: UIApplication, handleEventsForBackgroundURLSession identifier: String, completionHandler: @escaping () -> Void) {
+        if identifier == kBackgroundSessionIdentifier {
+            BridgeSDK.restoreBackgroundSession(identifier, completionHandler: completionHandler)
+        }
+    }
 
     // ------------------------------------------------
     // MARK: RootViewController management
